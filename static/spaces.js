c8ec1bd21be095eb22514fcf715e6be8c3cd1090
Shinmera <shinmera@tymoon.eu>
2023-06-29 15:25:11 +0200

    Better page editor

--- static/spaces.js
+++ static/spaces.js
@@ -1,13 +1,67 @@
 var Spaces = function(){
     var self = this;
-    var injectionArea = document.querySelector("#spaces-injection");
-    var form = injectionArea.querySelector("form.editor");
-    var init = false;
-    self.path = window.location.pathname;
+    var injectionArea;
+    var form;
+    var init;
+    var setupPage;
+    self.path = null;
     self.editor = null;
 
     if(document.querySelector("meta[name=path]"))
         self.path = document.querySelector("meta[name=path]").getAttribute("content");
+    
+    var loadPage = function(url, data, options){
+        return new Promise(function(ok, fail){
+            var request = new XMLHttpRequest();
+            for(const [key, value] of Object.entries(options || {})){
+                request[key] = value;
+            }
+            request.onerror = function(ev){
+                if(console) console.log("Spaces request failed", request, ev);
+                fail(request, ev);
+            };
+            request.onload = function(ev){
+                if(request.status == 200){
+                    if(console) console.log("Spaces request succeeded", request);
+                    ok(request.response);
+                }else{
+                    if(console) console.log("Spaces request failed", request, ev);
+                    fail(request, ev);
+                }
+            };
+            request.open("POST", url);
+            request.send(data);
+        });
+    };
+
+    var ajaxForm = function(form){
+        return new Promise(function(ok, fail){
+            form.addEventListener("submit", function(ev){
+                ev.preventDefault();
+                [].forEach.call(form.querySelectorAll("[type=submit]"), function(el){
+                    el.disabled = true;
+                });
+                var data = new FormData(form);
+                data.delete("browser");
+                
+                loadPage(form.getAttribute("action"), data, {responseType: 'json'})
+                    .then(function(response){
+                        [].forEach.call(form.querySelectorAll("[type=submit]"), function(el){
+                            el.removeAttribute("disabled");
+                        });
+                        ok(response);
+                    })
+                    .catch(fail);
+            });
+        });
+    };
+
+    var standardAjaxForm = function(form){
+        return ajaxForm(form)
+            .catch(function(request){
+                alert(request.response["message"]);
+            });
+    };
 
     var initEdit = function(){
         if(!form){
@@ -19,7 +73,7 @@ var Spaces = function(){
 <textarea name="content"></textarea>
 <input type="hidden" name="path" value="index.html">
 <input type="hidden" name="browser" value="true">
-<input type="submit" value="Save">`;
+<input type="submit" value="Save" title="Saves the content on the server and refreshes the display.">`;
             injectionArea.appendChild(form);
 
             var uploader = document.createElement("form");
@@ -29,25 +83,11 @@ var Spaces = function(){
             uploader.innerHTML = `\
 <input type="file" name="files[]" multiple required>
 <input type="hidden" name="browser" value="true">
-<input type="submit" value="Upload">`;
+<input type="submit" value="Upload" title="Uploads the file under its filename.">`;
             injectionArea.appendChild(uploader);
+            ajaxForm(uploader);
         }
         form.querySelector("input[name=path]").value = self.path;
-    };
-
-    var loadPage = function(url, data){
-        return new Promise(function(ok, fail){
-            var request = new XMLHttpRequest();
-            request.addEventListener("error", fail);
-            request.addEventListener("load", function(ev){
-                if(request.status == 200)
-                    ok(request.response);
-                else
-                    fail(ev);
-            });
-            request.open("POST", url);
-            request.send(data);
-        });
     };
 
     var loadTag = function(tag, attrs){
@@ -77,6 +117,57 @@ var Spaces = function(){
             ]);});
     };
 
+    var replacePageContent = function(content){
+        if(console) console.log("Replacing page content...");
+        // parse content
+        let newDocument = document.implementation.createHTMLDocument();
+        newDocument.open();
+        newDocument.write(content);
+        newDocument.close();
+        
+        // clear out all current page content **except** the injection.
+        var prune;
+        prune = function(root){
+            // Have to copy children first as we'll be modifying during iteration.
+            var children = [].slice.call(root.children);
+            [].forEach.call(children, function(el){
+                if(el === injectionArea){
+                }else if(!el.contains(injectionArea)){
+                    el.remove();
+                }else{
+                    prune(el);
+                }
+            });
+        };
+        prune(document);
+
+        // reconstruct anxilliary page content around the injection.
+        var transfer;
+        var srcInjectionArea = newDocument.querySelector("#spaces-injection");
+        transfer = function(src, dst){
+            [].forEach.call(src.children, function(el){
+                if(el === srcInjectionArea){
+                }else if(!el.querySelector("#spaces-injection")){
+                    dst.appendChild(el.cloneNode(true));
+                }else{
+                    var dstElement = el.cloneNode(false);
+                    dst.appendChild(dstElement);
+                    transfer(el, dstElement);
+                }
+            });
+        };
+        transfer(newDocument.children[0], document.children[0]);
+        
+        // Done.
+    };
+
+    var refreshEditor = function(){
+        return loadPage(window.location.href).then(function(v){
+            self.editor.setValue(v);
+            return self.editor;
+        });
+    };
+
     var initEditor = function(){
         injectionArea.classList.add("open");
         if(self.editor == null){
@@ -89,30 +180,47 @@ var Spaces = function(){
             });
             form.addEventListener("submit", function(){
                 form.querySelector("textarea").value = self.editor.getValue();
+                replacePageContent(self.editor.getValue());
             });
-            return loadPage(window.location.href)
-                .then(function(v){self.editor.setValue(v); return self.editor;});
+            ajaxForm(form);
+            return refreshEditor();
         }
         return Promise.resolve(self.editor);
     };
 
-    initEdit();
-    injectionArea.addEventListener("click", function(ev){ ev.stopPropagation(); });
-    injectionArea.querySelector("a.edit").addEventListener("click", function(ev){
-        ev.preventDefault();
-        if(!init)
-            initDependencies().then(initEditor);
-        else
-            initEditor();
+    var initSidebar = function(){
+        injectionArea.addEventListener("click", function(ev){ ev.stopPropagation(); });
+        injectionArea.querySelector("a.edit").addEventListener("click", function(ev){
+            ev.preventDefault();
+            if(!init)
+                initDependencies().then(initEditor);
+            else
+                initEditor();
 
-        var listen;
-        listen = function(){
-            document.removeEventListener("click", listen);
-            injectionArea.classList.remove("open");
-        };
-        document.addEventListener("click", listen);
-    });
+            var listen;
+            listen = function(){
+                document.removeEventListener("click", listen);
+                injectionArea.classList.remove("open");
+            };
+            document.addEventListener("click", listen);
+        });
+    };
 
+    setupPage = function(){
+        injectionArea = document.querySelector("#spaces-injection");
+        form = injectionArea.querySelector("form.editor");
+        init = false;
+        self.path = window.location.pathname;
+        self.editor = null;
+
+        if(document.querySelector("meta[name=path]"))
+            self.path = document.querySelector("meta[name=path]").getAttribute("content");
+        
+        initEdit();
+        initSidebar();
+    };
+
+    setupPage();
     return self;
 };
 window.addEventListener('DOMContentLoaded', function(){
